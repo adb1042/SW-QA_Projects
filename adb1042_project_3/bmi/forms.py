c5e104fbce23f02c3e91b6473527cd2cbd9c2985
adb1042 <98436533+adb1042@users.noreply.github.com>
2023-04-11 15:50:51 -0500

    updated the code to be baby proof

--- adb1042_project_3/bmi/forms.py
+++ adb1042_project_3/bmi/forms.py
@@ -3,6 +3,7 @@ from django import forms
 # creating a form
 class InputForm(forms.Form):
 
-	feet = forms.IntegerField(help_text="If you are 6'2\". You will enter your height as Feet:6 Inches: 2")
-	inches = forms.IntegerField()
-	weight = forms.IntegerField()
+	feet = forms.IntegerField(min_value=1,  help_text="If you are 6'2\". You will enter your height as Feet:6 Inches: 2")
+	inches = forms.IntegerField(min_value=-11)
+	weight = forms.IntegerField(min_value=1)
+	
