042eddab64e062fce429f18889cbcd801e09776b
adb1042 <98436533+adb1042@users.noreply.github.com>
2023-04-10 23:33:44 -0500

    Delete Project_3/Project_3 directory

--- Project_3/Project_3/bmi/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-# creating a form
-class InputForm(forms.Form):
-
-	feet = forms.IntegerField(help_text="If you are 6'2\". You will enter your height as Feet:6 Inches: 2")
-	inches = forms.IntegerField()
-	weight = forms.IntegerField()
