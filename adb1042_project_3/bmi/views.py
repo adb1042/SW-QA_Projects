43d31e0ddd4b8c42d76272eb305794731b7690a7
adb1042 <98436533+adb1042@users.noreply.github.com>
2023-04-11 16:47:12 -0500

    Added finishing touches to functional code

--- adb1042_project_3/bmi/views.py
+++ adb1042_project_3/bmi/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import InputForm
+from .forms import BMIForm
 from django.contrib import messages
 def calcBMI(feet,inches,weight):
 	w = weight * 0.45
@@ -9,25 +9,46 @@ def calcBMI(feet,inches,weight):
 	B = round(B,1)
 	return B
 
+def BMI_AVG(B):
+	if(B < 18.5):
+		x = "You Are Under Weight."
+		return(x)
+	if(B >=18.5 and B<=24.9):
+		x="You Are A Normal Weight."
+		return(x)
+	if(B>=25 and B <= 29.9):
+		x="You Are Over Weight."
+		return(x)
+	if(B>=30):
+		x = "You Are Obese."
+		return(x)
+		
+
+
+
+
+
+
+
 
 
 # Create your views here.
 def calculate(request):
 	BMI = 0
 	if request.method == 'POST':
-		form=InputForm(request.POST)
+		form=BMIForm(request.POST)
 		if form.is_valid():
 			feet = form.cleaned_data.get('feet')
 			inches = form.cleaned_data.get('inches')
 			weight = form.cleaned_data.get('weight')
-			messages.success(request, f'Feet:{feet}')
-			messages.success(request, f'Inches:{inches}')
-			messages.success(request, f'Weight:{weight}')
 			BMI = calcBMI(feet,inches,weight)
 			messages.success(request, f'Your BMI is:{BMI}')
+			getThiccStatus = BMI_AVG(BMI)
+			messages.success(request, f'{getThiccStatus}')
+
 
 
 	else:
-		form = InputForm()
+		form = BMIForm()
 
 	return render(request, 'bmi/calculate.html',{'form':form})
